5492fe1a2de66f64911d83349f553cfbb7a2f60d
klcuur <klcugur@outlook.com.tr>
2024-02-01 01:36:59 +0300

    Add project files.

---
+++ AspClientApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using AspClientApp.Dtos;
+using AspClientApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+
+
+namespace AspClientApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public async Task< IActionResult> Index()
+		{
+			var products = new List<ProductDto>();
+
+			using (var httpClient=new HttpClient())
+			{
+				using( var response=await httpClient.GetAsync("https://localhost:7207/api/products"))
+				{
+					string apiResponse=await response.Content.ReadAsStringAsync();
+					products=JsonSerializer.Deserialize<List<ProductDto>>(apiResponse);
+				}
+			}
+			return View(products);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ AspClientApp/Dtos/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AspClientApp.Dtos
+{
+	public class ProductDto
+	{
+		[JsonPropertyName("productId")]
+		public int ProductId {  get; set; }
+
+        [JsonPropertyName("productName")]
+        public string ProductName { get; set; } = null!;
+
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+	}
+}
